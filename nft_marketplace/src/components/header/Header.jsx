0c79feb7f841bde11d20f69010bb0ef2339f15fb
Aman <amanvermaspace@gmail.com>
2022-11-13 12:59:57 +0530

    collected assets and setup the index styling file

---
+++ nft_marketplace/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <button class="btn btn-primary">Hello</button>
+            <button class="btn btn-secondary">Hello</button>
+            <button class="btn btn-cta">Hello</button>
+        </div>
+    )
+}
+
+export default Header;
