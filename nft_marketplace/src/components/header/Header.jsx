ce58298fd4f0dc0c8005f17beed68978dfcaf86b
unknown <amanvermaspace@gmail.com>
2022-11-14 21:35:47 +0530

    modified jsx files

--- nft_marketplace/src/components/header/Header.jsx
+++ nft_marketplace/src/components/header/Header.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import './header.css';
+import logoImage from '../../assets/fenti-removebg-preview.png';
+import {FiMenu} from 'react-icons/fi'
+import {GrClose} from 'react-icons/gr';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-            <button class="btn btn-primary">Hello</button>
-            <button class="btn btn-secondary">Hello</button>
-            <button class="btn btn-cta">Hello</button>
-        </div>
+        <header id="header">
+            <div className="container header__container">
+
+            {/* logo */}
+                <div className="header__image">
+                    <img src={logoImage} alt="logo"/>
+                </div>
+            {/*  Links */}
+                <div className="header__links">
+                    <a href="#explore">Explore</a>
+                    <a href="#howitworks">How it works</a>
+                    <a href="#community">Community</a>
+                    <a href="#signin">Sign In</a>
+                    <button className="btn btn-secondary">Create</button>
+                </div>
+                <div className="mobile__navbar__button">
+                    <FiMenu className="nav__icon" name="menu-icon"/>
+                    <GrClose className="nav__icon" name="close-icon"/>
+                </div>
+            </div>
+
+        </header>
     )
 }
 
