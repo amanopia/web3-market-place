7b49df9b3323c563e4cc5817055ebdbf1d179e51
unknown <amanvermaspace@gmail.com>
2022-11-19 13:05:35 +0530

    created smaller card components, currently with fixed width

---
+++ nft_marketplace/src/components/cards-smaller/CardSmaller.jsx
@@ -0,0 +1,27 @@
+import './cards-smaller.css'
+
+const CardSmaller = (props) => {
+    return (
+        <div id="smaller__card__container">
+            <div className="smaller__card">
+                {/* 1 */}
+                <img src={props.image} alt="im1" />
+                {/* 2 */}
+                <div className="smaller__card__text">
+                    <h4>{props.title}</h4>
+                    <small>By {props.creator}</small>
+                    <div className="bid">
+                        <div>
+                            <small>Current Bid</small>
+                            <p>${props.price}</p>
+                        </div>
+                        <button className='btn bid__btn'>Place Bid</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardSmaller;
+
