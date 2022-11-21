292b81fd820e21a991836fafab6c16502288b72e
unknown <amanvermaspace@gmail.com>
2022-11-21 21:12:20 +0530

    Added basic styling to the header

--- nft_marketplace/src/components/top-collection/Collection.jsx
+++ nft_marketplace/src/components/top-collection/Collection.jsx
@@ -1,9 +1,17 @@
 import './collection.css'
+import {FaChevronDown} from 'react-icons/fa';
 
 const Collection = () => {
     return (
         <section className="container  collection__container" id="collection">
-            <h2>Our Top Collection in <span className="gradient">24 Hours</span></h2>
+            <div className='collection__header'>
+                <div>
+                    <h2>Our Top Collection in <span className="gradient">24 Hours</span></h2>
+                    <FaChevronDown className='arrow__gradient'/>
+                </div>
+                <p>View Ranking</p>
+            </div>
+            
         </section>
     )
 }
