6fd009cc5f6841fd1d060559398dead22e59d9b2
unknown <amanvermaspace@gmail.com>
2022-11-23 20:43:40 +0530

    created placeholder elements for collection data and prepared basic layout

--- nft_marketplace/src/components/Top-collection-cards/TopCollection.jsx
+++ nft_marketplace/src/components/Top-collection-cards/TopCollection.jsx
@@ -10,28 +10,36 @@ import Image4 from '../../assets/crown.png'
 const CollectionData = (props) => {
     return (
         <div className="collection__data">
+            {/* 1 */}
             <div className='number'>
-                {props.number}
+                {/* {props.number} */}
+                <p>1</p>
             </div>
+            {/* 2 */}
             <div className='collection__image'>
-                <img src={props.image} alt="someImg" />
+                {/* <img src={props.image} alt="someImg" /> */}
+                <img src={Image2} alt="someImg" />
             </div>
+            {/* 3 */}
             <div className='top__collection__data'>
                 <div className='collection__data__upper'>
-                    <h4>{props.title}</h4>
-                    <h4>{props.percentage}</h4>
+                    {/* <h4>{props.title}</h4> */}
+                    <h4>Trippin Ape</h4>
+                    {/* <h4>{props.percentage}</h4> */}
+                    <h4>+64.30%</h4>
                 </div>
                 <div className='collection__data__lower'>
                     <div className='side__left'>
                         <small>Floor Price</small>
                         <CgArrowsExchangeAltV />
-                        <small>{props.rateFraction}</small>
+                        {/* <small>{props.rateFraction}</small> */}
+                        <small>34.2</small>
                     </div>
                     <div className='side__right'>
                         <CgArrowsExchangeAltV />
-                        <small>{props.rate}</small>
+                        {/* <small>{props.rate}</small> */}
+                        <small>2382.32</small>
                     </div>
-
                 </div>
 
             </div>
