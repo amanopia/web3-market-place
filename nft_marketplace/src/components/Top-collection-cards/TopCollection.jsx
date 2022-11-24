e889e9e52fe61ea9501ad846120627f588edb6e2
unknown <amanvermaspace@gmail.com>
2022-11-25 01:14:51 +0530

    redid top collection section

--- nft_marketplace/src/components/Top-collection-cards/TopCollection.jsx
+++ nft_marketplace/src/components/Top-collection-cards/TopCollection.jsx
@@ -1,47 +1,39 @@
 import './TopCollection.css'
 import {CgArrowsExchangeAltV} from 'react-icons/cg';
-import Image from '../../assets/1125.webp'
-import Image2 from '../../assets/bayc.jpg'
-import Image3 from '../../assets/bayc2.jpg'
-import Image4 from '../../assets/crown.png'
+
 
 
 
 const CollectionData = (props) => {
     return (
-        <div className="collection__data">
-            {/* 1 */}
+        <div className='collection__data'>
+            {/* Number */}
             <div className='number'>
-                {/* {props.number} */}
-                <p>1</p>
+                <p>{props.number}</p>
             </div>
-            {/* 2 */}
+            {/* Image */}
             <div className='collection__image'>
-                {/* <img src={props.image} alt="someImg" /> */}
-                <img src={Image2} alt="someImg" />
+                <img src={props.source} alt="img"/>
             </div>
-            {/* 3 */}
-            <div className='top__collection__data'>
-                <div className='collection__data__upper'>
-                    {/* <h4>{props.title}</h4> */}
-                    <h4>Trippin Ape</h4>
-                    {/* <h4>{props.percentage}</h4> */}
-                    <h4>+64.30%</h4>
+            {/* Data */}
+            <div className='data'>
+                {/* Data row name */}
+                <div>
+                    <h4>{props.heading}</h4>
+                    <h4>{props.percentage}</h4>
                 </div>
-                <div className='collection__data__lower'>
-                    <div className='side__left'>
-                        <small>Floor Price</small>
+                {/* Data row price*/}
+                <div>
+                    <div className='floor__price__data'>
+                        <small>Floor Price: </small>
                         <CgArrowsExchangeAltV />
-                        {/* <small>{props.rateFraction}</small> */}
-                        <small>34.2</small>
+                        <small>{props.num}</small>
                     </div>
-                    <div className='side__right'>
+                    <div className='result__data'>
                         <CgArrowsExchangeAltV />
-                        {/* <small>{props.rate}</small> */}
-                        <small>2382.32</small>
+                        <small>{props.result}</small>
                     </div>
                 </div>
-                
             </div>
         </div>
     )
--- nft_marketplace/src/components/top-collection/Collection.jsx
+++ nft_marketplace/src/components/top-collection/Collection.jsx
@@ -1,6 +1,12 @@
 import './collection.css'
 import {FaChevronDown} from 'react-icons/fa';
 import CollectionData from '../Top-collection-cards/TopCollection';
+
+import Image from '../../assets/1125.webp'
+import Image2 from '../../assets/bayc.jpg'
+import Image3 from '../../assets/bayc2.jpg'
+import Image4 from '../../assets/crown.png'
+
 const Collection = () => {
     return (
         <section className="container  collection__container" id="collection">
@@ -11,17 +17,11 @@ const Collection = () => {
                 </div>
                 <a href="#asda">View Ranking</a>
             </div>
-            <div className='collection__data'>
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
-                <CollectionData />
+            <div className='small__cards__container'>
+                <CollectionData number="1" source={Image} heading="Trippin" percentage="67.80%" num="0.32" result="363.3"/>
+                
             </div>
+
             
         </section>
     )
