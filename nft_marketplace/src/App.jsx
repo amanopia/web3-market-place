c2750da2c85a1a83a691eb300b6b63c28f055f8d
unknown <amanvermaspace@gmail.com>
2022-11-26 21:31:11 +0530

    basic layout done

--- nft_marketplace/src/App.jsx
+++ nft_marketplace/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Banner />
       <Trending />
-      {/* <Discover /> */}
+      <Discover />
       <Collection />
       <CheckCollection />
     </>
--- nft_marketplace/src/components/check-collections/Check.jsx
+++ nft_marketplace/src/components/check-collections/Check.jsx
@@ -1,8 +1,38 @@
+import './check.css'
 const CheckCollection = () => {
     return (
-        <div className="container" id="check__collections">
+        <div className="container collection" id="check__collections">
             <div className="header">
                 <h2>Check the Collections</h2>
+            </div>
+            <div className="check__items">
+                <div className="section__one">
+                    <img src="" alt="something"/>
+                </div>
+                <div className="section__two">
+                    <div>
+                        <img src="" alt="something"/>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="something" />
+                        <div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="something" />
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="section__three">
+                    <h3>TOP COLLECTION OVER</h3>
+                    <h4>Last 7 days</h4>
+                </div>
             </div>
         </div>
     )
