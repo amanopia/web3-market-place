e1018bdd755f43eb2eb71ceb13dd9e71c0d7d3ce
unknown <amanvermaspace@gmail.com>
2022-12-04 23:39:20 +0530

    set the aspect ratio of the logo

--- nft_marketplace/src/components/footer/Footer.jsx
+++ nft_marketplace/src/components/footer/Footer.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './footer.css';
-
+import Logo from '../../assets/fenti-removebg-preview.png'
 const Footer = () => {
   return (
     <section className="container footer__container">
-      <div><p>Apple</p></div>
+      {/* Icon and email box */}
+      <div className="contact__holder">
+        <img src={Logo} alt="site-logo" />
+      </div>
       <div className="links__container">
-      <p>Apple</p>
+
       </div>
     </section>
   )
