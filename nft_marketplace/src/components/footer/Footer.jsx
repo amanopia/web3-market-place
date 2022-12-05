f6cfdd169e7162dab9e6dd1da2cab211bd206656
unknown <amanvermaspace@gmail.com>
2022-12-05 21:10:25 +0530

    prepared basic layout for the footer with all the elements entered

--- nft_marketplace/src/components/footer/Footer.jsx
+++ nft_marketplace/src/components/footer/Footer.jsx
@@ -7,9 +7,39 @@ const Footer = () => {
       {/* Icon and email box */}
       <div className="contact__holder">
         <img src={Logo} alt="site-logo" />
+        <p>Enter your email to get notified by fenti for latest updates</p>
+        <input type="text" placeholder='Email address'/>
       </div>
+      {/* Links */}
       <div className="links__container">
-
+        <div className="links__column">
+          <div className="links__heading">Fenti</div>
+          <a>Explore</a>
+          <a>All NFTs</a>
+          <a>About</a>
+        </div>
+        <div className="links__column">
+          <div className="links__heading">My Account</div>
+          <a>Profile</a>
+          <a>Favourites</a>
+          <a>Watchlist</a>
+          <a>My Collections</a>
+          <a>Settings</a>
+        </div>
+        <div className="links__column">
+          <div className="links__heading">Resources</div>
+          <a>Platform Status</a>
+          <a>Partners</a>
+          <a>Taxes</a>
+          <a>Newsletter</a>
+        </div>
+        <div className="links__column">
+          <div className="links__heading">Community</div>
+          <a>Help Center</a>
+          <a>Fenti Token</a>
+          <a>Suggest Feature</a>
+          <a>Subscribe</a>
+        </div>
       </div>
     </section>
   )
