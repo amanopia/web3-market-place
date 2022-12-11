9fb1a69cac21267084ff41ee0012af38bca277d5
unknown <amanvermaspace@gmail.com>
2022-12-12 02:14:20 +0530

    made some changes to path because of hofting failure on netlify

--- nft_marketplace/src/App.jsx
+++ nft_marketplace/src/App.jsx
@@ -1,10 +1,12 @@
-import Header from './components/header/Header';
-import Banner from './components/banner/Banner';
-import Trending from './components/trending/Trending'
-import Discover from './components/discover/Discover';
-import Collection from './components/top-collection/Collection';
-import CheckCollection from './components/check-collections/Check';
-import Footer from './components/footer/Footer';
+import Banner from '../src/components/banner/Banner';
+// import Header from './components/header/Header';
+// import Header from './components/header/Header'
+import Header from '../src/components/header/Header'
+import Trending from '../src/components/trending/Trending'
+import Discover from '../src/components/discover/Discover';
+import Collection from '../src/components/top-collection/Collection';
+import CheckCollection from '../src/components/check-collections/Check';
+import Footer from '../src/components/footer/Footer';
 import TestComponent from './components/testComponent/TestComponent';
 
 function App() {
