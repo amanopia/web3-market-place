b1b14a03ab09344b81be9a0ee8352427bd596ea8
unknown <amanvermaspace@gmail.com>
2022-12-12 02:45:05 +0530

    restored relative importts

--- nft_marketplace/src/App.jsx
+++ nft_marketplace/src/App.jsx
@@ -1,10 +1,10 @@
-import Header from '/components/header/Header.jsx';
-import Banner from '/components/banner/Banner.jsx';
-import Trending from '/components/trending/Trending.jsx'
-import Discover from '/components/discover/Discover.jsx';
-import Collection from '/components/top-collection/Collection.jsx';
-import CheckCollection from '/components/check-collections/Check.jsx';
-import Footer from '/components/footer/Footer.jsx';
+import Header from './components/header/Header.jsx';
+import Banner from './components/banner/Banner.jsx';
+import Trending from './components/trending/Trending.jsx'
+import Discover from './components/discover/Discover.jsx';
+import Collection from './components/top-collection/Collection.jsx';
+import CheckCollection from './components/check-collections/Check.jsx';
+import Footer from './components/footer/Footer.jsx';
 // import TestComponent from './components/testComponent/TestComponent';
 
 function App() {
